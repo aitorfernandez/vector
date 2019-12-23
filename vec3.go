8a4cd5084bafd7e93df9e8cbd37f0eb08f6cda3f
Aitor <helloimaitor@gmail.com>
2019-12-23 16:50:36 +0000

    add Add func

--- vec3.go
+++ vec3.go
@@ -9,3 +9,32 @@ type Vec3 struct {
 func (v *Vec3) ToString() string {
 	return fmt.Sprintf("Vec3 struct: %.2f, %.2f, %.2f", v.X, v.Y, v.Z)
 }
+
+func (v *Vec3) Add(a ...interface{}) {
+	switch e := a[0].(type) {
+	case Vec3:
+		v.X += e.X
+		v.Y += e.Y
+		v.Z += e.Z
+	case int, float64:
+		for i, arg := range a {
+			switch i {
+			case 0:
+				v.X += getFloat(arg)
+			case 1:
+				v.Y += getFloat(arg)
+			case 2:
+				v.Z += getFloat(arg)
+			}
+		}
+	}
+}
+
+func getFloat(i interface{}) float64 {
+	switch i.(type) {
+	case int:
+		return float64(i.(int))
+	default:
+		return i.(float64)
+	}
+}
--- vec3_test.go
+++ vec3_test.go
@@ -11,3 +11,27 @@ func TestToString(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	check := func(v1, v2 Vec3) {
+		if v1 != v2 {
+			t.Errorf("got %v want %v", v1, v2)
+		}
+	}
+
+	t.Run("add Vec3", func(t *testing.T) {
+		got := Vec3{2, 4, 5}
+		got.Add(Vec3{X: 3, Y: 1})
+		want := Vec3{5, 5, 5}
+
+		check(got, want)
+	})
+
+	t.Run("add float64", func(t *testing.T) {
+		got := Vec3{3, 6, 7}
+		got.Add(1, 1, 1)
+		want := Vec3{4, 7, 8}
+
+		check(got, want)
+	})
+}
